023ed670bea992a95fa5835e1f9c6a89e50e031b
prcnet <34053091+prcnet@users.noreply.github.com>
2018-01-25 14:21:59 -0800

    You are using the wrong resource file
    And thus you are throwing a client side error trying to load a file that does not exist. This resolves it.

--- vrp_job_display/__resource.lua
+++ vrp_job_display/__resource.lua
@@ -1,12 +1,8 @@
 
-description "vRP holdup"
+description "vRP job display"
 --ui_page "ui/index.html"
 
 dependency "vrp"
-
-client_scripts{ 
-  "client.lua"
-}
 
 server_scripts{ 
   "@vrp/lib/utils.lua",
