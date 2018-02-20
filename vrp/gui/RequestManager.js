cbcb69844b81ed1c78e0c7b059781aa36979dca8
Original Gamers <35341113+OriginalGamers@users.noreply.github.com>
2018-02-20 14:21:06 -0500

    Fix visual text for accept/decline

--- vrp/gui/RequestManager.js
+++ vrp/gui/RequestManager.js
@@ -13,7 +13,7 @@ function RequestManager()
 
 RequestManager.prototype.buildText = function(text,time)
 {
-  return "<span>"+text+" <span class=\"yes\">[F5]</span><span class=\"no\">[F6]</span>["+time+"s]</span>";
+  return "<span>"+text+" <span class=\"yes\">[NumPad+]</span><span class=\"no\">[NumPad-]</span>["+time+"s]</span>";
 }
 
 RequestManager.prototype.addRequest = function(id,text,time)
