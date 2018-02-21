408e287c1bb19077add5d5394288e7ab6b9be148
DunkoUK <31360770+DunkoUK@users.noreply.github.com>
2018-02-21 09:54:04 +0000

    Removed task_god to avoid null errors
    This only affects those who had god mode on prior to the update as it's removed.

--- vrp/modules/admin.lua
+++ vrp/modules/admin.lua
@@ -412,18 +412,18 @@ vRP.registerMenuBuilder("main", function(add, data)
 end)
 
 -- admin god mode
-function task_god()
-  SetTimeout(10000, task_god)
+-- function task_god()
+  -- SetTimeout(10000, task_god)
 
-  for k,v in pairs(vRP.getUsersByPermission("admin.god")) do
-    vRP.setHunger(v, 0)
-    vRP.setThirst(v, 0)
+  -- for k,v in pairs(vRP.getUsersByPermission("admin.god")) do
+    -- vRP.setHunger(v, 0)
+    -- vRP.setThirst(v, 0)
 
-    local player = vRP.getUserSource(v)
-    if player ~= nil then
-      vRPclient.setHealth(player, {200})
-    end
-  end
-end
+    -- local player = vRP.getUserSource(v)
+    -- if player ~= nil then
+      -- vRPclient.setHealth(player, {200})
+    -- end
+  -- end
+-- end
 
-task_god()
+-- task_god()
