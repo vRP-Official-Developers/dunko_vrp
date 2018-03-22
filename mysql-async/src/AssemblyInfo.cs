c848985c816b81915a40fcc9498957ee483fca49
DunkoUK <31360770+DunkoUK@users.noreply.github.com>
2018-03-22 11:00:42 +0000

    Updated MySQL-Async to V2.0.4

--- mysql-async/src/AssemblyInfo.cs
+++ mysql-async/src/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Numéro de build
 //      Révision
 //
-[assembly: AssemblyVersion("2.0.2.0")]
-[assembly: AssemblyFileVersion("2.0.2.0")]
+[assembly: AssemblyVersion("2.0.4.0")]
+[assembly: AssemblyFileVersion("2.0.4.0")]
--- mysql-async/src/MySQLAsync.cs
+++ mysql-async/src/MySQLAsync.cs
@@ -2,13 +2,6 @@
 using CitizenFX.Core.Native;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Data;
-using System.Data.Common;
-using System.Diagnostics;
 using MySql.Data.MySqlClient;
 
 namespace MySQLAsync
