9c56ce98327e6fb23946be3cf4ad16d867cfdec2
DunkoUK <31360770+DunkoUK@users.noreply.github.com>
2020-06-12 20:22:24 +0100

    Amended previous

--- Start_Server.cmd
+++ Start_Server.cmd
@@ -1,6 +1,9 @@
 @echo off
 echo -
-echo FXServer VRP
+echo Get a server at www.zap-hosting.com/Dunko
+pause
+echo 10% off with code Dunko
+pause
 echo -
 pause
 start C:\FxServer\FxServer.exe +exec server.cfg
