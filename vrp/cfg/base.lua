1075fe366da9d0e60e0ae837eab7731a0ed707d4
JamesUK2020 <60899490+JamesUK2020@users.noreply.github.com>
2020-12-30 23:07:33 +0000

    update: leave notice in base.lua config about db change

--- vrp/cfg/base.lua
+++ vrp/cfg/base.lua
@@ -2,12 +2,17 @@
 local cfg = {}
 
 -- mysql credentials
-cfg.db = {
-  host = "127.0.0.1", -- database ip (default is local)
-  database = "vrpfx",   -- name of database
-  user = "root",    --  database username
-  password = ""   -- password of your database
-}
+-- cfg.db = {
+--   host = "127.0.0.1", -- database ip (default is local)
+--   database = "vrpfx",   -- name of database
+--   user = "root",    --  database username
+--   password = ""   -- password of your database
+-- }
+
+--[[
+  THE ABOVE DB CREDENTIALS ARE NO LONGER USED. 
+  Go to ghmattimysql/config.json and enter your database details
+]]
 
 cfg.save_interval = 30 -- seconds
 cfg.whitelist = false -- enable/disable whitelist
