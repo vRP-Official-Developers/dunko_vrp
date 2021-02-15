689b2c064627c0660f212cf4d113df12259bfa3b
JamesUK2020 <60899490+JamesUK2020@users.noreply.github.com>
2021-02-15 18:31:06 +0000

    add: rageui garage update

--- vrp/sharedcfg/options.lua
+++ vrp/sharedcfg/options.lua
@@ -5,3 +5,6 @@ vRPConfig.SurvivalUiEnabled = true; -- Controls the UI under the healthbar.
 vRPConfig.EnableComa = true; -- Controls the vRP coma on death.
 vRPConfig.EnableFoodAndWater = true; -- Controls the food and water system.
 vRPConfig.EnableHealthRegen = true; -- Controls the health regen. (Whether they regen health after taking damage do not disable if coma is enabled.)
+vRPConfig.EnableBuyVehicles = true; -- Enables ability to buy vehicles from the RageUI Garages.  
+vRPConfig.LoadPreviews = true; -- Controls the car previews with the RageUI Garages.
+vRPConfig.VehicleStoreRadius = 250; -- Controls radius a vehicle can be stored from.
