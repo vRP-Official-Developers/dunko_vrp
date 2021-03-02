a2a92758243a03769b1c39daad52df9437ef6eb7
JamesUK2020 <60899490+JamesUK2020@users.noreply.github.com>
2021-03-02 21:52:31 +0000

    add: car spawn to misc section

--- vrp/cfg/admin_menu.lua
+++ vrp/cfg/admin_menu.lua
@@ -119,5 +119,45 @@ cfg.MiscButtons = {
     ["Noclip"] = {function(self)
         tvRP.toggleNoclip({})
     end, "player.noclip", "Teleports you to a waypoint"},
+    ["Spawn Vehicle"] = {function(self)
+        AddTextEntry('FMMC_MPM_NC', "Enter the car spawncode name")
+        DisplayOnscreenKeyboard(1, "FMMC_MPM_NC", "", "", "", "", "", 30)
+        while (UpdateOnscreenKeyboard() == 0) do
+            DisableAllControlActions(0);
+            Wait(0);
+        end
+        if (GetOnscreenKeyboardResult()) then
+            local result = GetOnscreenKeyboardResult()
+            if result then 
+                local mhash = GetHashKey(result)
+                local i = 0
+                while not HasModelLoaded(mhash) and i < 10000 do
+                RequestModel(mhash)
+                Citizen.Wait(10)
+                i = i+1
+                    if i > 150 then 
+                        tvRP.notify('~r~Model could not be loaded!')
+                        break 
+                    end
+                end
+                -- spawn car
+                if HasModelLoaded(mhash) then
+                    local x,y,z = tvRP.getPosition()
+                    if pos then
+                        x,y,z = table.unpack(pos)
+                    end
+                    local nveh = CreateVehicle(mhash, x,y,z+0.5, 0.0, true, false)
+                    SetVehicleOnGroundProperly(nveh)
+                    SetEntityInvincible(nveh,false)
+                    SetPedIntoVehicle(GetPlayerPed(-1),nveh,-1) -- put player inside
+                    SetVehicleNumberPlateText(nveh, "P "..tvRP.getRegistrationNumber())
+                    --Citizen.InvokeNative(0xAD738C3085FE7E11, nveh, true, true) -- set as mission entity
+                    SetVehicleHasBeenOwnedByPlayer(nveh,true)
+                    local nid = NetworkGetNetworkIdFromEntity(nveh)
+                    SetNetworkIdCanMigrate(nid,cfg.vehicle_migration)
+                end
+            end
+        end
+    end, "admin.spawnveh", "Spawns you a car"},
 }
 return cfg
