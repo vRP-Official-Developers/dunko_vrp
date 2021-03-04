b89794f006ecc2fa6a614fbef926b82d82db9bba
JamesUK2020 <60899490+JamesUK2020@users.noreply.github.com>
2021-03-04 23:06:12 +0000

    add: warningID serverside

--- vrp/modules/warningsystem.lua
+++ vrp/modules/warningsystem.lua
@@ -24,6 +24,7 @@ function getvrpWarnings(user_id,source)
 		newdate = tonumber(date) / 1000
 		newdate = os.date('%Y-%m-%d', newdate)
 		warningTable["warning_date"] = newdate
+        warningTable["warning_id"] = warningID
 	end
 	return vrpwarningstables
 end
@@ -43,22 +44,28 @@ AddEventHandler("vrp:warnPlayer",function(target_id,adminName,warningReason)
 	if vRP.hasPermission(user_id,"player.kick") then
 		warning = "Warning"
 		warningDate = getCurrentDate()
-		exports['GHMattiMySQL']:execute("INSERT INTO vrp_warnings (`warning_id`, `user_id`, `warning_type`, `duration`, `admin`, `warning_date`, `reason`) VALUES (NULL, @user_id, @warning_type, 0, @admin, @warning_date,@reason);", {user_id = target_id,warning_type = warning, admin = adminName, warning_date = warningDate, reason = warningReason}, function() end)
+		exports['ghmattimysql']:execute("INSERT INTO vrp_warnings (`user_id`, `warning_type`, `duration`, `admin`, `warning_date`, `reason`) VALUES (@user_id, @warning_type, 0, @admin, @warning_date,@reason);", {user_id = target_id,warning_type = warning, admin = adminName, warning_date = warningDate, reason = warningReason}, function() end)
 	else
 		vRPclient.notify(player,{"~r~no perms to warn player"})
 	end
 end)
 
+function saveWarnLog(target_id,adminName,warningReason)
+	warning = "Warning"
+	warningDate = getCurrentDate()
+	exports['ghmattimysql']:execute("INSERT INTO vrp_warnings (`user_id`, `warning_type`, `duration`, `admin`, `warning_date`, `reason`) VALUES (@user_id, @warning_type, 0, @admin, @warning_date,@reason);", {user_id = target_id,warning_type = warning, admin = adminName, warning_date = warningDate, reason = warningReason}, function() end)
+end
+
 function saveKickLog(target_id,adminName,warningReason)
 	warning = "Kick"
 	warningDate = getCurrentDate()
-	exports['GHMattiMySQL']:execute("INSERT INTO vrp_warnings (`warning_id`, `user_id`, `warning_type`, `duration`, `admin`, `warning_date`, `reason`) VALUES (NULL, @user_id, @warning_type, 0, @admin, @warning_date,@reason);", {user_id = target_id,warning_type = warning, admin = adminName, warning_date = warningDate, reason = warningReason}, function() end)
+	exports['ghmattimysql']:execute("INSERT INTO vrp_warnings (`user_id`, `warning_type`, `duration`, `admin`, `warning_date`, `reason`) VALUES (@user_id, @warning_type, 0, @admin, @warning_date,@reason);", {user_id = target_id,warning_type = warning, admin = adminName, warning_date = warningDate, reason = warningReason}, function() end)
 end
 
 function saveBanLog(target_id,adminName,warningReason,warning_duration)
 	warning = "Ban"
 	warningDate = getCurrentDate()
-	exports['GHMattiMySQL']:execute("INSERT INTO vrp_warnings (`warning_id`, `user_id`, `warning_type`, `duration`, `admin`, `warning_date`, `reason`) VALUES (NULL, @user_id, @warning_type, @duration, @admin, @warning_date,@reason);", {user_id = target_id,warning_type = warning, admin = adminName, duration = warning_duration, warning_date = warningDate, reason = warningReason}, function() end)
+	exports['ghmattimysql']:execute("INSERT INTO vrp_warnings (`user_id`, `warning_type`, `duration`, `admin`, `warning_date`, `reason`) VALUES (@user_id, @warning_type, @duration, @admin, @warning_date,@reason);", {user_id = target_id,warning_type = warning, admin = adminName, duration = warning_duration, warning_date = warningDate, reason = warningReason}, function() end)
 end
 
 
