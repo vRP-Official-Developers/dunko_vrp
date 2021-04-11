60056de21b27654efb15ee1905c740ac335a8696
JamesUK2020 <60899490+JamesEU@users.noreply.github.com>
2021-04-11 19:07:49 +0100

    add: more commands

--- vrp_discordbot/commands/warnings.js
+++ vrp_discordbot/commands/warnings.js
@@ -3,15 +3,15 @@ const Discord = require('discord.js');
 const fs = require('fs');
 
 exports.runcmd = (fivemexports, client, message, params) => {
-    if (!params[0]) {
+    if (!params[0] || !parseInt(params[0])) {
         return message.reply('Invalid args! Correct term is: ' + process.env.PREFIX + 'warnings [permid]')
     }
     fivemexports.ghmattimysql.execute("SELECT * FROM `vrp_warnings` WHERE user_id = ?", [params[0]], (result) => {
         var table = new AsciiTable('F10 Warnings')
-        table.setHeading('Warning Type', 'Duration', 'Reason', 'Admin', 'Date')
+        table.setHeading('Warning ID', 'Warning Type', 'Duration', 'Reason', 'Admin', 'Date')
         for (i = 0; i < result.length; i++) {
             var date = new Date(+result[i].warning_date)
-            table.addRow(result[i].warning_type, result[i].duration, result[i].reason, result[i].admin, date.toDateString())
+            table.addRow(result[i].warning_id, result[i].warning_type, result[i].duration, result[i].reason, result[i].admin, date.toDateString())
         }
         message.channel.send('```ascii\n' + table.toString() + '```').catch(err => {
             fs.writeFile(`${client.path}/warnings_${params[0]}.txt`, table.toString(), function(err) {
