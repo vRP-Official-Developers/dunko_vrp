c95b3de4a243cdb7812c31cf42bcd40e9df5783c
JamesUK2020 <60899490+JamesEU@users.noreply.github.com>
2021-04-11 12:30:24 +0100

    tweak: files

--- vrp_discordbot/commands/ban.js
+++ vrp_discordbot/commands/ban.js
@@ -1,15 +1,13 @@
 exports.runcmd = (fivemexports, client, message, params) => {
     if (!params[0] || !params[1]) {
-        return message.reply('Invalid args! Correct term is: ' + process.env.PREFIX + 'kick [permid] [reason]')
+        return message.reply('Invalid args! Correct term is: ' + process.env.PREFIX + 'ban [permid] [time (hours)] [reason]')
     }
     const reason = params.slice(1).join(' ');
-    fivemexports.vrp.vrpbot('getUserSource', [parseInt(params[0])], function(d) {
-        let newval = fivemexports.vrp.vrpbot('kick', [d, `You were kicked from the server via Discord for: ${reason} | Kicking Admin: ${message.author.username} (${message.author.id})`])
-        message.reply(`Success! Kicked Player: ${params[0]} UserID`)
-    })
+    let newval = fivemexports.vrp.vrpbot('banConsole', [params[0], params[1], `${reason}\n\nBanning Discord Admin: ${message.author.username} (${message.author.id})\n`])
+    message.reply(`Success! Banned Player: ${params[0]} UserID`)
 }
 
 exports.conf = {
-    name: "kick",
+    name: "ban",
     perm: 2
 }
