e0d23aa6e029146aace7093796b97ae3e54a9e4b
JamesUK <60899490+JamesEU@users.noreply.github.com>
2021-07-08 23:12:25 +0100

    change file names

--- vrp_discordbot/commands/removegroup.js
+++ vrp_discordbot/commands/removegroup.js
@@ -1,18 +1,18 @@
 exports.runcmd = (fivemexports, client, message, params) => {
     if (!params[0] || !parseInt(params[0]) || !params[1]) {
-        return message.reply('Invalid args! Correct term is: ' + process.env.PREFIX + 'addgroup [permid] [group name]')
+        return message.reply('Invalid args! Correct term is: ' + process.env.PREFIX + 'removegroup [permid] [group name]')
     }
     fivemexports.ghmattimysql.execute("SELECT * FROM `vrp_user_data` WHERE user_id = ?", [params[0]], (result) => {
         if (result.length > 0) {
             let dvalue = JSON.parse(result[0].dvalue)
             let groups = dvalue.groups
-            groups[params[1]] = true;
+            groups[params[1]] = undefined;
             fivemexports.ghmattimysql.execute("UPDATE `vrp_user_data` SET dvalue = ? WHERE user_id = ?", [JSON.stringify(dvalue), params[0]])
         }
     })
     let embed = {
-        "title": "Added Group",
-        "description": `\nSuccess! Added Group to: ${params[0]} Group Name: ${params[1]}`,
+        "title": "Removed Group",
+        "description": `\nSuccess! Removed Group to: ${params[0]} Group Name: ${params[1]}`,
         "color": 5301186,
         "footer": {
             "text": "vRP • Made by JamesUK#6793"
@@ -23,6 +23,6 @@ exports.runcmd = (fivemexports, client, message, params) => {
 }
 
 exports.conf = {
-    name: "addgroup",
+    name: "removegroup",
     perm: 3
 }
