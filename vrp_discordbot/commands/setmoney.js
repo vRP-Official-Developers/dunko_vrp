7c5c68b181b4a65c1295ccc9160b2b02a70b5b6f
Developer Caz <76732321+Caz-Developer@users.noreply.github.com>
2021-07-12 19:59:39 +0100

    Create setmoney.js

---
+++ vrp_discordbot/commands/setmoney.js
@@ -0,0 +1,28 @@
+exports.runcmd = (fivemexports, client, message, params) => {
+    if (!params[0] || !parseInt(params[0]) || !params[1]) {
+        return message.reply('Invalid args! Correct term is: ' + process.env.PREFIX + 'addgroup [permid] [amount]')
+    }
+    fivemexports.ghmattimysql.execute("SELECT * FROM `vrp_user_data` WHERE user_id = ?", [params[0]], (result) => {
+        if (result.length > 0) {
+            let dvalue = JSON.parse(result[0].dvalue)
+            let groups = dvalue.groups
+            groups[params[1]] = true;
+            fivemexports.ghmattimysql.execute("UPDATE `vrp_user_moneys` SET waller = ? WHERE user_id = ?", [JSON.stringify(dvalue), params[0]])
+        }
+    })
+    let embed = {
+        "title": "Cash Has Been Set",
+        "description": `\nSuccess! Set Money Of: ${params[0]} To ${params[1]}`,
+        "color": 5301186,
+        "footer": {
+            "text": "vRP • Made by JamesUK#6793"
+        },
+        "timestamp": new Date()
+    }
+    message.channel.send({ embed })
+}
+
+exports.conf = {
+    name: "setmoney",
+    perm: 3
+}
