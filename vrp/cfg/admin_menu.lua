6727049c15b3d2547d3b499d37f98cf86fa0af1e
JamesUK <60899490+JamesEU@users.noreply.github.com>
2021-07-12 14:52:21 +0100

    fix: indentation

--- vrp/cfg/admin_menu.lua
+++ vrp/cfg/admin_menu.lua
@@ -112,7 +112,6 @@ cfg.Buttons = {
             tvRP.notify('Please enter a warning message!')
         end
     end, "player.kick"},
-    
     ["Show Warnings"] = {function(self)
     ExecuteCommand("showwarnings "..tostring(self))
     end, "player.kick"},
