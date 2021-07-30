e814b6caac03cf116b379765d7dd04bbd9ceba80
Developer Caz <76732321+Caz-Developer@users.noreply.github.com>
2021-07-30 18:36:42 +0100

    Added "getmyid", "getmytempid" chat commands
    Added Commands

--- vrp/base.lua
+++ vrp/base.lua
@@ -321,7 +321,13 @@ function vRP.ReLoadChar(source)
         end
     end)
 end
+RegisterCommand("getmyid", function(source)
+    TriggerClientEvent('chatMessage', source, "[Server]", {255, 255, 255}, " Perm ID: " .. vRP.getUserId({source}) , "alert")
+end)
 
+RegisterCommand("getmytempid", function(source)
+	TriggerClientEvent('chatMessage', source, "[Server]", {255, 255, 255}, " Your Temp-ID: " .. source, "alert")
+end)
 -- This can only be used server side and is for the vRP bot. 
 exports("vrpbot", function(method_name, params, cb)
     if cb then 
