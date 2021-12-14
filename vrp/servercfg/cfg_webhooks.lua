1d2a19eb2c81f4d58e987b1e05a335eb1577e050
Callum <76732321+dev-callum@users.noreply.github.com>
2021-12-14 16:22:09 +0000

    Update cfg_webhooks.lua

--- vrp/servercfg/cfg_webhooks.lua
+++ vrp/servercfg/cfg_webhooks.lua
@@ -1,18 +1,20 @@
 local webhooks = {}
+-- THESE LOGS CAN BE TURNED OFF BY CHANGING THE VALUE TO 'NONE'
+-- THESE LOGS ONLY LOG user_id + source_id
+
+
 
 
 -- ADMIN LOGS
-webhooks.banlog = 'test'
-webhooks.kicklog = 'test'
-webhooks.warnlog = 'test'
+
+webhooks.banlog = 'none'
+webhooks.kicklog = 'none'
+webhooks.warnlog = 'none'
 
 -- JOIN/LEAVE LOGS
-webhooks.joinlog
-webhooks.leavelog
 
-
-
-print(webhooks.banlog)
+webhooks.joinlog = 'none'
+webhooks.leavelog = 'none'
 
 
 
