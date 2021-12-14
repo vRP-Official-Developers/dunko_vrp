86e9613058b6264317873ccb282061983f9aa0bd
Callum <76732321+dev-callum@users.noreply.github.com>
2021-12-14 12:47:52 +0000

    Create cfg_webhooks.lua

---
+++ vrp/servercfg/cfg_webhooks.lua
@@ -0,0 +1,20 @@
+local webhooks = {}
+
+
+-- ADMIN LOGS
+webhooks.banlog = 'test'
+webhooks.kicklog = 'test'
+webhooks.warnlog = 'test'
+
+-- JOIN/LEAVE LOGS
+webhooks.joinlog
+webhooks.leavelog
+
+
+
+print(webhooks.banlog)
+
+
+
+
+return webhooks
